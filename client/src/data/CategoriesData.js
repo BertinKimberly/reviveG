1cc2837f8c3e4ff805417e642269e72eacf3daae
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-09-27 19:00:05 +0200

    set up file tree

---
+++ client/src/data/CategoriesData.js
@@ -0,0 +1,30 @@
+export const CategoriesData = [
+   {
+      _id: "1",
+      title: "Romantic",
+   },
+   {
+      _id: "2",
+      title: "Action",
+   },
+   {
+      _id: "3",
+      title: "Horror",
+   },
+   {
+      _id: "4",
+      title: "Comedy",
+   },
+   {
+      _id: "5",
+      title: "Adventure",
+   },
+   {
+      _id: "6",
+      title: "Fantasy",
+   },
+   
+
+   
+
+];
---
+++ client/src/screens/WatchPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WatchPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WatchPage
