25b7e1b20d82fb8aa3ce4e298c5d97380ddec064
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-10-06 10:10:41 +0200

    done with getMovies route

--- server/controllers/MoviesController.js
+++ server/controllers/MoviesController.js
@@ -8,3 +8,46 @@ export const importMovies = asyncHandler(async (req, res) => {
    const movies = await Movie.insertMany(MoviesData);
    res.status(201).json(movies);
 });
+
+export const getMovies = asyncHandler(async (req, res) => {
+   try {
+      //filter movies by category,time,lang,rate,year and search
+
+      const { category, time, language, rate, year, search } = req.query;
+
+      let query = {
+         ...(category && { category }),
+         ...(time && { time }),
+         ...(language && { language }),
+         ...(rate && { rate }),
+         ...(year && { year }),
+         ...(search && { name: { $regex: search, $options: "i" } }),
+      };
+
+      //load more movies
+
+      const page = Number(req.query.pageNumber) || 1;
+      const limit = 2;
+      const skip = (page - 1) * limit;
+
+      //find movies by query, skip and limit
+
+      const movies = await Movie.find(query)
+         .sort({ createdArt: -1 })
+         .skip(skip)
+         .limit(limit);
+
+      //get total number of mobies
+      const count = await Movie.countDocuments(query);
+
+      //send response with movies and total number of movies
+      res.json({
+         movies,
+         page,
+         pages: Math.ceil(count / limit),
+         totalMovies: count,
+      });
+   } catch (error) {
+      res.status(400).json({ message: error.message });
+   }
+});
--- server/routes/movieRouter.js
+++ server/routes/movieRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 
 import { protect, admin } from "../middlewares/Auth.js";
-import { importMovies } from "../controllers/MoviesController";
+import { getMovies, importMovies } from "../controllers/MoviesController";
 
 const router = express.Router();
 
 // ****************PUBLIC ROUTES*******************
-router.post("/", importMovies);
+router.post("/import", importMovies);
+router.post("/", getMovies);
 
 // ***********************PRIVATE ROUTES****************************
 
