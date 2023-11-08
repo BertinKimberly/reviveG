6d92af18531f9bb347f4cdc6342616ca2369a1e9
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-11-08 18:22:16 +0200

    update styles of Footer

--- reviveG-client/src/Layout/Footer/Footer.jsx
+++ reviveG-client/src/Layout/Footer/Footer.jsx
@@ -60,7 +60,7 @@ const Footer = () => {
       },
    ];
    return (
-      <div className='bg-dry py-4 border-t-2 border-black'>
+      <div className='bg-dry py-4 border-t-2 border-black pb-[5rem]'>
          <div className='container mx-auto px-2'>
             <div className='grid grid-cols-2 md:grid-cols-7 xl:grid-cols-12 gap-5 sm:gap-9 lg:gap-11 xl:gap-7 py-10 justify-between'>
                {Links.map((link, index) => (
@@ -105,7 +105,7 @@ const Footer = () => {
             <hr className='w-3/4 ' />
          </div>
 
-         <p className="text-center mt-2 text-border">
+         <p className="text-center text-border my-3">
             &copy;<span className="mx-2">2023</span>reviveG
          </p>
       </div>
