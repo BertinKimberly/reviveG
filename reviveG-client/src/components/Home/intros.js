8c4eebf03f72b4ea76cb5e5452dcdf623d2ccb3e
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-11-21 11:12:54 +0200

    updated banner

--- reviveG-client/src/components/Home/intros.js
+++ reviveG-client/src/components/Home/intros.js
@@ -1,3 +1,3 @@
 export const intros=[
-    "Welcome to reviveG","Enjoy watching unlimited GOSPEL movies for free","Know more about gospel by the help of reviveG"
+    "Welcome to reviveG","Enjoy watching unlimited GOSPEL movies for free","Know more about gospel by the help of reviveG","Watch online or download to watch offline","Like Movies, comment on movies and Rate movies ","Have fun"
 ]
